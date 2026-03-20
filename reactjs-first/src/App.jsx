2c27d5374d3702a2b0bfde13ca01f7aa292b6b3a
Bharat Rajoriya <Bharat Rajoriya>
2026-03-21 00:50:05 +0530

    working-card

--- reactjs-first/src/App.jsx
+++ reactjs-first/src/App.jsx
@@ -1,28 +1,13 @@
 import ('../src/App.css')
 import Card from './component/Card'
 
-const cardData = JSON.parse[
-  {
-    "name": "Card 1",
-    "description": "This is the description for Card 1."
-  },
-  {
-    "name": "Card 2",
-    "description": "This is the description for Card 2."
-  },
-  {
-    "name": "Card 3",
-    "description": "This is the description for Card 3."
-  }
-]
-
 const App = () => {
   return (
     <>
     <div className="container_hero">
-      <Card title={cardData[0].name} description={cardData[0].description} />
-      <Card title={cardData[1].name} description={cardData[1].description} />
-      <Card title={cardData[2].name} description={cardData[2].description} />
+      <Card title="Card Title 1" description="This is the description for card 1." />
+      <Card title="Card Title 2" description="This is the description for card 2." />
+      <Card title="Card Title 3" description="This is the description for card 3." />
     </div>
       
     </>
