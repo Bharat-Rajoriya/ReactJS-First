7cb702f99fddda541b973ad0712f07c39a325095
Bharat Rajoriya <Bharat Rajoriya>
2026-03-20 23:31:50 +0530

    first commit

---
+++ reactjs-first/src/App.jsx
@@ -0,0 +1,14 @@
+
+const App = () => {
+  return (
+    <>
+    <div className="container_hero">
+      <h1>Hello World</h1>
+      <p>This is my first React App</p>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
