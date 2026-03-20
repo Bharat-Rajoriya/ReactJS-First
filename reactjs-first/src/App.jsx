d4cd09bfeaa6b5a90b1f14e78e18317035708cd5
Bharat Rajoriya <Bharat Rajoriya>
2026-03-21 00:39:16 +0530

    card-creation

--- reactjs-first/src/App.jsx
+++ reactjs-first/src/App.jsx
@@ -1,11 +1,28 @@
 import ('../src/App.css')
+import Card from './component/Card'
+
+const cardData = JSON.parse[
+  {
+    "name": "Card 1",
+    "description": "This is the description for Card 1."
+  },
+  {
+    "name": "Card 2",
+    "description": "This is the description for Card 2."
+  },
+  {
+    "name": "Card 3",
+    "description": "This is the description for Card 3."
+  }
+]
 
 const App = () => {
   return (
     <>
     <div className="container_hero">
-      <h1>Hello World</h1>
-      <p>This is my first React App</p>
+      <Card title={cardData[0].name} description={cardData[0].description} />
+      <Card title={cardData[1].name} description={cardData[1].description} />
+      <Card title={cardData[2].name} description={cardData[2].description} />
     </div>
       
     </>
